cb16ae863f28d1b947c5c1124bc94d92abf5a0d4
Kartik Singhal <kartiks025@gmail.com>
2018-08-02 11:30:45 +0530

    Adding colormap to images

--- assignment1/1/code/myMainScript.m
+++ assignment1/1/code/myMainScript.m
@@ -6,13 +6,20 @@ tic;
 img = imread('../data/circles_concentric.png');
 figure('Name','Original Image');
 imshow(img);
+colormap(jet(200))
 
 figure('Name','shrinked image(d=2)');
 img2 = myShrinkImageByFactorD(img,2);
 imshow(img2);
+colormap(jet(200))
 
 figure('Name','shrinked image(d=3)');
 img3 = myShrinkImageByFactorD(img,3);
 imshow(img3);
+colormap(jet(200))
 
+img = imread('../data/barbaraSmall.png');
+figure('Name','Original Image');
+imshow(img);
+colormap(jet(200))
 toc;
