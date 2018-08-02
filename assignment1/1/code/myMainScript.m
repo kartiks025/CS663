2d3bf258a86c558643ceb06f766d435722a277aa
vishwajeet97 <vishwajeetsingh29797@gmail.com>
2018-08-02 11:45:58 +0530

    Added axis and colormap to images

--- assignment1/1/code/myMainScript.m
+++ assignment1/1/code/myMainScript.m
@@ -6,17 +6,23 @@ tic;
 img = imread('../data/circles_concentric.png');
 figure('Name','Original Image');
 imshow(img);
+axis on
 colormap(jet(200))
+colorbar
 
 figure('Name','shrinked image(d=2)');
 img2 = myShrinkImageByFactorD(img,2);
 imshow(img2);
+axis on
 colormap(jet(200))
+colorbar
 
 figure('Name','shrinked image(d=3)');
 img3 = myShrinkImageByFactorD(img,3);
 imshow(img3);
+axis on
 colormap(jet(200))
+colorbar
 
 img = imread('../data/barbaraSmall.png');
 figure('Name','Original Image');
