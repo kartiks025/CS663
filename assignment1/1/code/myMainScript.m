aa459f16081abcff823f206b028efa5e70397adf
Kartik Singhal <kartiks025@gmail.com>
2018-08-02 12:16:53 +0530

    A1-1 done

--- assignment1/1/code/myMainScript.m
+++ assignment1/1/code/myMainScript.m
@@ -3,23 +3,33 @@
 tic;
 %% Your code here
 
-img = imread('../data/circles_concentric.png');
+% img = imread('../data/circles_concentric.png');
+% figure('Name','Original Image');
+% imshow(img);
+% colormap(jet(200))
+% 
+% figure('Name','shrinked image(d=2)');
+% img2 = myShrinkImageByFactorD(img,2);
+% imshow(img2);
+% colormap(jet(200))
+% 
+% figure('Name','shrinked image(d=3)');
+% img3 = myShrinkImageByFactorD(img,3);
+% imshow(img3);
+% colormap(jet(200))
+
+img4 = imread('../data/barbaraSmall.png');
 figure('Name','Original Image');
-imshow(img);
-colormap(jet(200))
+imshow(img4);
+% colormap(jet(200))
 
-figure('Name','shrinked image(d=2)');
-img2 = myShrinkImageByFactorD(img,2);
-imshow(img2);
-colormap(jet(200))
+% figure('Name','Enlarged Image');
+% img5 = myBilinearInterpolation(img4);
+% imshow(mat2gray(img5));
+% % colormap(jet(200))
 
-figure('Name','shrinked image(d=3)');
-img3 = myShrinkImageByFactorD(img,3);
-imshow(img3);
-colormap(jet(200))
-
-img = imread('../data/barbaraSmall.png');
-figure('Name','Original Image');
-imshow(img);
-colormap(jet(200))
+figure('Name','Nearest Neighbor Enlarged Image');
+img6 = myNearestNeighborInterpolation(img4);
+imshow(mat2gray(img6));
+% colormap(jet(200))
 toc;
