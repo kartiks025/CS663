0efe790a41fc855d0823b1b8dade3d221c1db7fd
vishwajeet97 <vishwajeetsingh29797@gmail.com>
2018-08-04 18:15:45 +0530

    Added code for AHE;debugging still left

--- assignment1/2/codes/myMainScript.m
+++ assignment1/2/codes/myMainScript.m
@@ -2,23 +2,23 @@
 
 tic;
 %% Your code here
-img = imread('../data/retina.png');
+img = imread('../data/barbara.png');
 figure('Name','Original Image');
 imshow(img);
 axis on
 colormap(jet(200))
 colorbar
 
-imgRef = imread('../data/retinaRef.png');
-figure('Name','Reference Image');
-imshow(imgRef);
-axis on
-colormap(jet(200))
-colorbar
+% imgRef = imread('../data/retinaRef.png');
+% figure('Name','Reference Image');
+% imshow(imgRef);
+% axis on
+% colormap(jet(200))
+% colorbar
 
 figure('Name','Contrast image');
-img2 = myHM(img,imgRef);
-imshow(mat2gray(img2));
+img2 = myAHE(img,25);
+imshow(img2);
 axis on
 colormap(jet(200))
 colorbar
