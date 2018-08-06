a2662583a64bf2dfde7b3d2530ede5d94f4f34c8
Kartik Singhal <kartiks025@gmail.com>
2018-08-06 23:55:18 +0530

    change colormap

--- assignment1/1/code/myMainScript.m
+++ assignment1/1/code/myMainScript.m
@@ -7,7 +7,7 @@ img = imread('../data/circles_concentric.png');
 imshow(img);
 title('Name','Original Image');
 axis on;
-colormap(jet(200));
+colormap(gray(200));
 colorbar;
 
 img2 = myShrinkImageByFactorD(img,2);
@@ -15,7 +15,7 @@ imshow(img2);
 title('Name','shrinked image(d=2)');
 
 axis on;
-colormap(jet(200));
+colormap(gray(200));
 colorbar;
 
 img3 = myShrinkImageByFactorD(img,3);
@@ -23,7 +23,7 @@ imshow(img3);
 title('Name','shrinked image(d=3)');
 
 axis on;
-colormap(jet(200));
+colormap(gray(200));
 colorbar;
 
 %% myBilinearInterpolation
@@ -33,7 +33,7 @@ imshow(img4);
 title('Name','Original Image');
 
 axis on;
-colormap(jet(200));
+colormap(gray(200));
 colorbar;
 
 img5 = myBilinearInterpolation(img4);
@@ -41,7 +41,7 @@ imshow(uint8(img5));
 title('Name','Bilinear Interpolated Image');
 
 axis on;
-colormap(jet(200));
+colormap(gray(200));
 colorbar;
 
 %% myNearestNeighborInterpolation
@@ -50,7 +50,7 @@ imshow(img4);
 title('Name','Original Image');
 
 axis on;
-colormap(jet(200));
+colormap(gray(200));
 colorbar;
 
 img6 = myNearestNeighborInterpolation(img4);
@@ -58,7 +58,7 @@ imshow(uint8(img6));
 title('Name','Nearest Neighbor Interpolated Image');
 
 axis on;
-colormap(jet(200));
+colormap(gray(200));
 colorbar;
 
 %% end
