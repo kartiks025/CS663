d1a9e5f68c235d338894f7f91e112dfc95cd2995
Kartik Singhal <kartiks025@gmail.com>
2018-08-15 05:57:43 +0530

    added assignment 2 statement

--- 150050025_150050046_150050106_assignment1_grayscale_transformation/1/code/myShrinkImageByFactorD.m
+++
@@ -1,4 +0,0 @@
-function shrinkImg = myShrinkImageByFactorD(input, dim)
-   [len,wid] = size(input);
-   shrinkImg = input(1:dim:len,1:dim:wid);
-end
--- 150050025_150050046_150050106_assignment1_grayscale_transformation/2/code/myDisplayImageColor.m
+++
@@ -1,7 +0,0 @@
-function myDisplayImageGrey(img,title1)
-	figure;
-    imshow (uint8(img));
-    title(title1);
-	colormap(jet(200));
-	axis on;
-	colorbar;
