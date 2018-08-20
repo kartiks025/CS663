9d3bdec24644d2a1647326a7504856293e3ea711
Kartik Singhal <kartiks025@gmail.com>
2018-08-20 18:30:04 +0530

    2nd part

--- assignment2/2/code/myMainScript.m
+++ assignment2/2/code/myMainScript.m
@@ -2,5 +2,17 @@
 
 tic;
 %% Your code here
+%% Bilateral filtering
+imageData = load('../data/barbara.mat');
+image = imageData.imageOrig;
+[len, wid] = size(image);
+corruptedImage = image + 0.05*(max(max(image))-min(min(image)))*randn(len);
+
+
+figure,imshow(mat2gray(image));
+out = myBilateralFiltering(corruptedImage, 1.3, 4, 5);
+rsmd = sqrt(sum(sum((out-image).^2))/(len*wid))
+figure,imshow(mat2gray(corruptedImage));
+figure,imshow(mat2gray(out));
 
 toc;
