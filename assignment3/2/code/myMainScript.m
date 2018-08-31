abebf6a7d20e417b713e44adc5b48fe363c5d61b
vishwajeet97 <vishwajeetsingh29797@gmail.com>
2018-08-31 21:32:47 +0530

    Mean Shift Segmentation coded up;Quite Possibly buggy

--- assignment3/2/code/myMainScript.m
+++ assignment3/2/code/myMainScript.m
@@ -2,5 +2,14 @@
 
 tic;
 %% Your code here
+imageOrig = imread('../data/baboonColor.png');
+imageOrig = imgaussfilt(imageOrig, 1.0);     
+imageOrig = imresize(imageOrig, 0.5);
+
+h1 = 0.02;
+h2 = 150;
+rate = 9.0;
+segmentImage = myMeanShiftSegmentation(imageOrig, h1, h2, rate);
+myDisplayTwoImage(imageOrig, segmentImage);
 
 toc;
